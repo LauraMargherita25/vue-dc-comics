60d080b94f80afc8f9a8ffed2a56813400da572d
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-22 16:35:42 +0100

    I'm not dead

---
+++ src/components/HeaderDC.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    {{ message }}
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderDC',
+  data () {
+    return {
+      message: 'Odio tutti dìsenza distinzioni'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

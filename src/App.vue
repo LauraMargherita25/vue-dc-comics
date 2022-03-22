378e20707d4873e409efc5e27896c5e03305b17c
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-22 18:25:25 +0100

    Added partials

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  background-color: #1c1c1c;
 }
+@import "./assets/styles/style.scss"
 </style>

956cca2fe4e0376a22d5858b79871510dd912e4e
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-23 16:42:36 +0100

    Added arrComics

---
+++ src/components/CardComic.vue
@@ -0,0 +1,20 @@
+.<template>
+  <div class="card">
+      <img :src="cardImg" :alt="cardText">
+      <p>{{ cardText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComic',
+  props: {
+    cardImg: String,
+    cardText: String
+  }
+}
+</script>
+
+<style>
+
+</style>
